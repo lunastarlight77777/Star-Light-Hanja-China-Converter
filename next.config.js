e26f82e23c842b63c3a53952854174524bae31f7
lunastarlight0327 <blogdangdang1214@gmail.com>
2026-05-19 16:43:06 +0900

    github

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Next.js 프로젝트를 정적 HTML/CSS/JS 파일로 변환해줍니다.
+  images: {
+    unoptimized: true, // GitHub Pages에서 이미지 로딩 에러가 나는 것을 방지합니다.
+  },
+};
+
 module.exports = nextConfig;
