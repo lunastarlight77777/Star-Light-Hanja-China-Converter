95c6976ef46c399c7b45c62779fd04b8191244ba
lunastarlight0327 <blogdangdang1214@gmail.com>
2026-05-20 19:35:00 +0900

    fix: api

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export' <- 이 줄은 반드시 지우거나 이렇게 주석 처리해야 합니다!
   images: {
-    unoptimized: true, // 이미지 최적화 비활성화는 그대로 두셔도 괜찮습니다.
+    unoptimized: true,
   },
 };
 
